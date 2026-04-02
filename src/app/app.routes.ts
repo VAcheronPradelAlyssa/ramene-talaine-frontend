1acae9e1c5063968b2bd2721e2978a70b6b9e947
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-02 11:19:09 +0200

    feat: add route guard for authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Inscription } from './components/inscription/inscription';
 import { Connexion } from './components/connexion/connexion';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', component: Home, canActivate: [authGuard] },
   { path: 'inscription', component: Inscription },
   { path: 'connexion', component: Connexion },
 ];
