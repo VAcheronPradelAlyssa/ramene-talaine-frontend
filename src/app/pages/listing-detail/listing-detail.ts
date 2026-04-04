96a20b393dd703ef8bd95b811e39e8aa9969045d
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-04 12:46:58 +0200

    feat: fetch listing details by id

--- src/app/pages/listing-detail/listing-detail.ts
+++ src/app/pages/listing-detail/listing-detail.ts
@@ -1,23 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ListingService } from '../../services/listing.service';
 import { Listing, ListingType } from '../../models/listing.model';
-import { AsyncPipe, NgIf, NgFor } from '@angular/common';
-import { Observable, switchMap } from 'rxjs';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-listing-detail',
   standalone: true,
-  imports: [CommonModule, AsyncPipe, NgIf, NgFor],
+  imports: [CommonModule, NgIf, NgFor],
   templateUrl: './listing-detail.html',
   styleUrl: './listing-detail.scss',
 })
-export class ListingDetail {
+export class ListingDetail implements OnInit {
   private route = inject(ActivatedRoute);
   private listingService = inject(ListingService);
 
-  id = this.route.snapshot.paramMap.get('id')!;
-  listing$: Observable<Listing> = this.listingService.getListingById(this.id);
+  id!: string;
+  listing?: Listing;
+  loading = true;
+  error?: string;
   listingType = ListingType;
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+    this.listingService.getListingById(this.id).subscribe({
+      next: (data) => {
+        this.listing = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Annonce introuvable ou erreur serveur.';
+        this.loading = false;
+      }
+    });
+  }
 }
