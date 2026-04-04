f148a6dbabcc3a2ddfde47d654bd2d70de1ab804
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-04 13:55:59 +0200

    feat: refactor listing detail component to improve error handling and loading state

--- src/app/pages/listing-detail/listing-detail.ts
+++ src/app/pages/listing-detail/listing-detail.ts
@@ -1,9 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ListingService } from '../../services/listing.service';
 import { Listing, ListingType } from '../../models/listing.model';
-import { NgIf, NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-listing-detail',
@@ -13,26 +13,39 @@ import { NgIf, NgFor } from '@angular/common';
   styleUrl: './listing-detail.scss',
 })
 export class ListingDetail implements OnInit {
-  private route = inject(ActivatedRoute);
-  private listingService = inject(ListingService);
-
-  id!: string;
   listing?: Listing;
   loading = true;
   error?: string;
   listingType = ListingType;
 
+  constructor(
+    private route: ActivatedRoute,
+    private listingService: ListingService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id')!;
-    this.listingService.getListingById(this.id).subscribe({
-      next: (data) => {
-        this.listing = data;
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (!id) {
+        this.error = 'Id manquant dans l’URL';
         this.loading = false;
-      },
-      error: (err) => {
-        this.error = 'Annonce introuvable ou erreur serveur.';
-        this.loading = false;
+        return;
       }
+      this.loading = true;
+      this.error = undefined;
+      this.listingService.getListingById(id).subscribe({
+        next: (data) => {
+          this.listing = data;
+          this.loading = false;
+          this.cdr.detectChanges();
+        },
+        error: () => {
+          this.error = 'Annonce introuvable ou erreur serveur.';
+          this.loading = false;
+          this.cdr.detectChanges();
+        }
+      });
     });
   }
 }
