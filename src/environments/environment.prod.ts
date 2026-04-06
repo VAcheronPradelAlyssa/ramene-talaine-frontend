520a71b184b53ee51d841753e4ced0ee67b170a2
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-07 01:36:53 +0200

    feat: mettre à jour l'URL de l'API pour l'environnement de production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://ramene-talaine-backend.railway.internal',
+  API_URL: 'https://ramene-talaine-backend-production.up.railway.app',
 };
